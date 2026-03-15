69c4c118c6a321dc878ca16ccbb4bf09a17d8cca
Syam Prasad <syamofficial1212@gmail.com>
2026-03-15 20:14:02 +0530

    Add Document Object Example HTML file

---
+++ week7/a.Documentobject.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Document Object Example</title>
+</head>
+
+<body>
+
+<h2 id="msg">Hello Student</h2>
+
+<script>
+
+document.write("Page Title: " + document.title + "<br>");
+
+let text = document.getElementById("msg").innerHTML;
+
+document.write("Heading Text: " + text);
+
+</script>
+
+</body>
+</html>
